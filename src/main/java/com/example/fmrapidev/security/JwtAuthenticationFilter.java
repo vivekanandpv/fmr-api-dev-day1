4cff735ecdc929a668935d9f48ff3a00633917e9
Vivekanand P V <vivekanandpv@outlook.com>
2025-01-31 17:19:16 +0530

    JwtAuthenticationFilter completion

--- src/main/java/com/example/fmrapidev/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/fmrapidev/security/JwtAuthenticationFilter.java
@@ -4,6 +4,10 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -11,12 +15,38 @@ import java.io.IOException;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final AuthenticationManager authenticationManager;
+
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
     @Override
     protected void doFilterInternal(
             HttpServletRequest request, 
             HttpServletResponse response, 
             FilterChain filterChain
     ) throws ServletException, IOException {
-        filterChain.doFilter(request, response);    //  blindly pass the authentication
+        String authorizationHeaderValue = request.getHeader("Authorization");
+
+        if (authorizationHeaderValue != null && authorizationHeaderValue.startsWith("Bearer ")) {
+            String jwt = authorizationHeaderValue.substring(7);
+
+            try {
+                Authentication authentication = new JwtAuthentication(jwt);
+                authentication = authenticationManager.authenticate(authentication);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+                filterChain.doFilter(request, response);
+            } catch (AuthenticationException ae) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            }
+        } else {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        }
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        return request.getServletPath().equals("/api/v1/auth/login");
     }
 }
