4fa229bcb7fcc80a50c5a287e8da2d9238fe0a72
Vivekanand P V <vivekanandpv@outlook.com>
2025-01-31 15:09:38 +0530

    LoginFailedException and handler

--- src/main/java/com/example/fmrapidev/apis/ExceptionHandlerAdvice.java
+++ src/main/java/com/example/fmrapidev/apis/ExceptionHandlerAdvice.java
@@ -1,8 +1,10 @@
 package com.example.fmrapidev.apis;
 
+import com.example.fmrapidev.exceptions.LoginFailedException;
 import com.example.fmrapidev.exceptions.RecordNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -16,6 +18,12 @@ public class ExceptionHandlerAdvice {
     @ExceptionHandler(RecordNotFoundException.class)
     public ResponseEntity<Object> handleRecordNotFoundException(RecordNotFoundException e) {
         logger.error(e.getMessage());
-        return ResponseEntity.status(404).body(Map.of("error", e.getMessage()));
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("error", e.getMessage()));
+    }
+
+    @ExceptionHandler(LoginFailedException.class)
+    public ResponseEntity<Map<String, String>> handleLoginFailedException(LoginFailedException exception) {
+        logger.error(exception.getMessage());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("error", "Could not process login"));
     }
 }
