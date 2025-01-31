e3c51f5a9dfef226a3e64d210cec8e41969ba007
Vivekanand P V <vivekanandpv@outlook.com>
2025-01-31 14:49:17 +0530

    implementing AppUserServiceImplementation

--- src/main/java/com/example/fmrapidev/services/AppUserServiceImplementation.java
+++ src/main/java/com/example/fmrapidev/services/AppUserServiceImplementation.java
@@ -1,16 +1,47 @@
 package com.example.fmrapidev.services;
 
+import com.example.fmrapidev.exceptions.RecordNotFoundException;
 import com.example.fmrapidev.models.AppUser;
+import com.example.fmrapidev.repositories.AppRoleRepository;
+import com.example.fmrapidev.repositories.AppUserRepository;
 import com.example.fmrapidev.viewmodels.UserRegisterViewModel;
+import org.springframework.beans.BeanUtils;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.stream.Collectors;
 
 public class AppUserServiceImplementation implements AppUserService {
+    private final AppUserRepository appUserRepository;
+    private final AppRoleRepository appRoleRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AppUserServiceImplementation(AppUserRepository appUserRepository, AppRoleRepository appRoleRepository, PasswordEncoder passwordEncoder) {
+        this.appUserRepository = appUserRepository;
+        this.appRoleRepository = appRoleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     public AppUser register(UserRegisterViewModel viewModel) {
-        return null;
+        AppUser entity = new AppUser();
+        BeanUtils.copyProperties(viewModel, entity, "password");
+
+        entity.setAppRoles(
+                viewModel
+                        .getRoles()
+                        .stream()
+                        .map(r -> this.appRoleRepository.findByRole(r).orElseThrow(RecordNotFoundException::new))
+                        .collect(Collectors.toSet())
+        );
+
+        entity.setPassword(this.passwordEncoder.encode(viewModel.getPassword()));
+
+        return appUserRepository.saveAndFlush(entity);
     }
 
     @Override
     public AppUser findUserByUsername(String username) {
-        return null;
+        return appUserRepository.findUserByUsername(username)
+                .orElseThrow(RecordNotFoundException::new);
     }
 }
